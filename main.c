c585b8660b11f5636d57b0f7abb06cc02d041b97
jlee0942 <55135113+jlee0942@users.noreply.github.com>
2019-10-10 15:58:51 +0900

    03

--- main.c
+++ main.c
@@ -24,6 +24,23 @@ int getmax(int a, int b)
 }
 
 int main(int argc, char *argv[]) {
+	int x,y;
+	int output;
+	
+	x=2;
+	y=5;
+	
+	output = sumTwo(x,y);
+	printf("sumTwo=%i\n", output);
+	
+	output = square(x);
+	printf("square=%i\n", output);
+	
+	output = getmax(x,y);
+	printf("getmax=%i\n", output);
+	
+	
+	
 
 	return 0;
 }
