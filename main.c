298e89507aae3f4868944d4750f8cefb38600d8c
jlee0942 <55135113+jlee0942@users.noreply.github.com>
2019-10-15 16:08:17 +0900

    07

--- main.c
+++ main.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int n);
+
+int fact(int n);
+int combination(int n, int r)
+{
+	int high, low;
+	high= fact(n);
+	low= fact(n-r)*fact(r);
+
+	return (high/low);
+}
+
+int fact(int n)
+{
+	int res=1;
+	int i;
+	
+	for(i=1; i<=n ;i++)
+	{
+		res=res*i;
+	}
+	return res;		
+}
+
 
 int main(void)
 {
-	int result;
-	result=square(5);
-	printf("%d", result);	
+	int n, r, result;
+	
+	printf("input n&r:");
+	scanf("%d %d", &n, &r);
+	result=combination(n,r);
+	printf("result=%d\n",result);
 }
 
-int square(int n)
-{
-	return(n*n);
-}
+
 
